bff9c7f9096381743aa48c804c40d559fa658c24
nastrandberg <n.a.strandberg@soton.ac.uk>
2022-11-02 16:53:46 +0100

    Addition of the RSL to the trends diagram and change of wording from "disturbnace" to "sucessional"

--- RSL_curve.R
+++ RSL_curve.R
@@ -1,2 +1,2 @@
 RSL<-read.csv("sea_level_interpolated.csv")
-plot(RSL, col="steelblue", lwd=0.5, xlab="Cal. years BP", ylab="m.a.s.l.", main="Relative sea level", cex=0.5)
+plot(RSL, col="steelblue", lwd=0.5, xlab="Cal. years BP", ylab="m.a.s.l.", cex=0.5)
